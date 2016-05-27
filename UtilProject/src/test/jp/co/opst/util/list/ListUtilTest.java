91e213b5e2eba5d160614e31f7734395bcce484a
ishino <ishino.y@opst.co.jp>
2016-05-27 18:03:08 +0900

    テストケースを追加

---
+++ UtilProject/src/test/jp/co/opst/util/list/ListUtilTest.java
@@ -0,0 +1,14 @@
+package jp.co.opst.util.list;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ListUtilTest {
+
+	@Test
+	public void testHasOnlyOneElement() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
