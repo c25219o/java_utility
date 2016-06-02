eb7a37b2f922d42c336f3d1052d837cfdc3f4aa2
ishino <ishino.y@opst.co.jp>
2016-06-02 18:58:02 +0900

    カラム変換クラスのテストクラスを作成（未実装）

---
+++ UtilProject/src/test/jp/co/opst/util/db_content/ColumnConvertUtilTest.java
@@ -0,0 +1,18 @@
+package jp.co.opst.util.db_content;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ColumnConvertUtilTest {
+
+    @Test
+    public void testPrintFieldFromCSVColumn() {
+        String[] columns = new String[1];
+        columns[0] = "GOODS_NUM, GOODS_NAME, GOODS_PRICE, GOODS_EXP, MAKER, REG_DATE, LST_UPDATE_TIME";
+
+        // TODO テスト方法考える
+        //ColumnConvertUtil.main(columns);
+    }
+
+}
