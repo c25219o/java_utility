dc965b987ff2ad03072107421a38ac67ed84bddf
ishino <ishino.y@opst.co.jp>
2016-06-07 15:40:35 +0900

    NumberFormatExceptionをcatchしない整数判別ユーティリティ

---
+++ UtilProject/src/main/jp/co/opst/util/literal/IntegerUtil.java
@@ -0,0 +1,27 @@
+package jp.co.opst.util.literal;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class IntegerUtil {
+
+	public static boolean isInteger(Object object) {
+        if (object == null) {
+            return false;
+        }
+
+		String str = object.toString();
+		if (str.contains(" ")) {
+			return false;
+        }
+
+		Pattern p = Pattern.compile("^[0-9]*$");
+		Matcher m = p.matcher(str);
+		return (m.find());
+	}
+
+	public static boolean isNotInteger(Object object) {
+		return !isInteger(object);
+    }
+
+}
