a9e6b23e981ca96b374406c88d048a4907ce4af9
ishino <ishino.y@opst.co.jp>
2016-06-15 15:23:31 +0900

    ユーティリティ追加・マージ

--- UtilProject/src/main/jp/co/opst/util/literal/StringUtil.java
+++ UtilProject/src/main/jp/co/opst/util/literal/StringUtil.java
@@ -4,11 +4,7 @@ public class StringUtil {
 
 	public static boolean isSame(String str1, String str2) {
 		if (str1 == null || str2 == null) {
-			if (str1 == null && str2 == null) {
-				return true;
-			} else {
-				return false;
-			}
+			return str1 == str2;
 		}
 		return str1.equals(str2);
 	}
